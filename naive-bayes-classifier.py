493edb778f038052d7b5ff253f5d882f2a9fb3bf
leoquiroa <leonelquiroa@gmail.com>
2020-12-23 09:27:27 -0600

    order the code

--- naive-bayes-classifier.py
+++ naive-bayes-classifier.py
@@ -3,7 +3,11 @@ from csv import reader
 from math import sqrt
 from math import exp
 from math import pi
- 
+
+#########################################################################
+# Data Preparation
+#########################################################################
+
 # Load a CSV file
 def load_csv(filename):
 	dataset = list()
@@ -15,12 +19,12 @@ def load_csv(filename):
 			dataset.append(row)
 	return dataset
  
-# Convert string column to float
+# Convert string columns to float
 def str_column_to_float(dataset, column):
 	for row in dataset:
 		row[column] = float(row[column].strip())
  
-# Convert string column to integer
+# Convert string class column to integer
 def str_column_to_int(dataset, column):
 	class_values = [row[column] for row in dataset]
 	unique = set(class_values)
@@ -31,7 +35,19 @@ def str_column_to_int(dataset, column):
 	for row in dataset:
 		row[column] = lookup[row[column]]
 	return lookup
- 
+
+#########################################################################
+# Split and calculate stats for each group
+#########################################################################
+
+# calculate statistics for each row
+def summarize_by_class(dataset):
+	separated = separate_by_class(dataset)
+	summaries = dict()
+	for class_value, rows in separated.items():
+		summaries[class_value] = summarize_dataset(rows)
+	return summaries
+
 # Split the dataset by class values, returns a dictionary
 def separate_by_class(dataset):
 	separated = dict()
@@ -42,7 +58,17 @@ def separate_by_class(dataset):
 			separated[class_value] = list()
 		separated[class_value].append(vector)
 	return separated
- 
+
+#########################################################################
+# Statistical Calculation
+#########################################################################
+
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+	summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
+	del(summaries[-1])
+	return summaries
+
 # Calculate the mean of a list of numbers
 def mean(numbers):
 	return sum(numbers)/float(len(numbers))
@@ -52,25 +78,20 @@ def stdev(numbers):
 	avg = mean(numbers)
 	variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
 	return sqrt(variance)
- 
-# Calculate the mean, stdev and count for each column in a dataset
-def summarize_dataset(dataset):
-	summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
-	del(summaries[-1])
-	return summaries
- 
-# Split dataset by class then calculate statistics for each row
-def summarize_by_class(dataset):
-	separated = separate_by_class(dataset)
-	summaries = dict()
-	for class_value, rows in separated.items():
-		summaries[class_value] = summarize_dataset(rows)
-	return summaries
- 
-# Calculate the Gaussian probability distribution function for x
-def calculate_probability(x, mean, stdev):
-	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
-	return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+#########################################################################
+# Prediction
+#########################################################################
+
+# Predict the class for a given row
+def predict(summaries, row):
+	probabilities = calculate_class_probabilities(summaries, row)
+	best_label, best_prob = None, -1
+	for class_value, probability in probabilities.items():
+		if best_label is None or probability > best_prob:
+			best_prob = probability
+			best_label = class_value
+	return best_label
  
 # Calculate the probabilities of predicting each class for a given row
 def calculate_class_probabilities(summaries, row):
@@ -83,27 +104,22 @@ def calculate_class_probabilities(summaries, row):
 			probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
 	return probabilities
  
-# Predict the class for a given row
-def predict(summaries, row):
-	probabilities = calculate_class_probabilities(summaries, row)
-	best_label, best_prob = None, -1
-	for class_value, probability in probabilities.items():
-		if best_label is None or probability > best_prob:
-			best_prob = probability
-			best_label = class_value
-	return best_label
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
+	return (1 / (sqrt(2 * pi) * stdev)) * exponent
  
 # Make a prediction with Naive Bayes on Iris Dataset
-filename = '/home/PyML/iris.csv'
-dataset = load_csv(filename)
-for i in range(len(dataset[0])-1):
-	str_column_to_float(dataset, i)
-# convert class column to integers
-str_column_to_int(dataset, len(dataset[0])-1)
-# fit model
-model = summarize_by_class(dataset)
-# define a new record
-row = [5.7,2.9,4.2,1.3]
-# predict the label
-label = predict(model, row)
-print('Data=%s, Predicted: %s' % (row, label))
+if __name__ == "__main__":    
+	# Data Preparation
+	filename = '/home/PyML/iris.csv'
+	dataset = load_csv(filename)
+	for i in range(len(dataset[0])-1):
+		str_column_to_float(dataset, i)
+	str_column_to_int(dataset, len(dataset[0])-1)
+	# split and calculate mean/stdev
+	model = summarize_by_class(dataset)
+	# define a new record and predict
+	row = [5.7,2.9,4.2,1.3]
+	label = predict(model, row)
+	print('Data=%s, Predicted: %s' % (row, label))
